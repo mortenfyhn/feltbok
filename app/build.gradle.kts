71b697edad38778c468ece0d000ae27d9f64a408
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2026-06-04 17:09:29 +0200

    Bump to versionCode 4 / versionName 0.4 for the v0.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.feltbok"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
         buildConfigField("String", "GIT_VERSION", "\"$gitVersion\"")
     }
 
