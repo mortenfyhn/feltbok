f3365f7f5877794b5ae3e53e58549927c906f8ad
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2026-06-04 13:20:23 +0200

    Bump to versionCode 2 / versionName 0.2 for the v0.2 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.feltbok"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
         buildConfigField("String", "GIT_VERSION", "\"$gitVersion\"")
     }
 
