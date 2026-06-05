3761e9ee153908a0e2526f66c7e208dacd4f53cb
Morten Fyhn Amundsen <morten.fyhn.amundsen@gmail.com>
2026-06-05 23:46:43 +0200

    Bump version to 0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.feltbok"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
         buildConfigField("String", "GIT_VERSION", "\"$gitVersion\"")
     }
 
